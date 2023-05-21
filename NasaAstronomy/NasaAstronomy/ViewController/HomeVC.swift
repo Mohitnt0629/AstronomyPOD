5c02a7dbbb5223d3c5891d434abc7f90a648cb82
Mohitnt0629 <134224991+Mohitnt0629@users.noreply.github.com>
2023-05-21 22:55:06 +0530

    Intia Commit

---
+++ NasaAstronomy/NasaAstronomy/ViewController/HomeVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NasaAstronomy
+//
+//  Created by netset on 19/05/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    //MARK: - IBOUTLET OF HOME VIEW PROPERTIES
+    @IBOutlet var baseVwProperties: HomeViewProperties!
+    
+    //MARK: - CREATE OBJECT OF VIEW MODEL
+    var objHomeVM = HomeVM()
+    
+    //MARK: - VIEW LIFE CYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.baseVwProperties.delegateMethod = self
+        DispatchQueue.main.async {
+            self.checkConnection(date: CommonMethod.shared.getStringFromDate(date: Date(), needFormat: DateFormate.yymmdd))
+        }
+    }
+}
+
